910caba81ecf17e8e64813d98046fdc7a131acb0
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2025-10-11 00:37:07 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form");
+const container = document.querySelector('#imgs')
+const div = document.querySelector(".img");
+const search = document.getElementById("search").value;
+const button = document.querySelector('.click')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+
+async function fetchData() {
+    try {
+        const search = document.getElementById("search");
+        container.innerHTML = ""
+        const response = await fetch(`https://api.unsplash.com/search/photos?query=${search.value}&client_id=YRJfxxZ8trO8zfBGKCMERyGykg8j9Eh1Xe557k-HV5o`)
+
+        if (!response.ok) {
+            throw new Error('Could not fetch resource')
+        }
+        const data = await response.json()
+
+        div.style.display = "none"
+        const result = data.results
+        const image = [...result]
+
+        for (const images of image) {
+            const img = document.createElement('img')
+            const link = document.createElement('a')
+            const url = images.urls.small
+            const id = images.id
+            container.append(link)
+            link.append(img)
+            img.src = url
+            img.id = `${id}`
+            link.href = url
+            link.target = "_blank"
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
